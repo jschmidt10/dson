302d6d088bdab569f628fd6f586e1c6b2a4ee078
jeff <jffry.schmidt@gmail.com>
2013-11-23 14:04:19 -0500

    dson now requires quotes around value for more stable parsing

--- glowtoe/src/test/groovy/org/dson/glowtoe/parser/DsonParserTest.groovy
+++ glowtoe/src/test/groovy/org/dson/glowtoe/parser/DsonParserTest.groovy
@@ -7,7 +7,7 @@ class DsonParserTest {
 
 	@Test
 	public void testValidParse() {
-		def dson = '{{name="Tom Test"},{age=20},{hourlyRate=10.50}}'
+		def dson = '{{name="Tom Test"},{age="20"},{hourlyRate="10.50"}}'
 		def map = new DsonParser(dson).parse()
 
 		Assert.assertEquals(3, map.size())
@@ -16,15 +16,33 @@ class DsonParserTest {
 		Assert.assertEquals('10.50', map.hourlyRate)
 	}
 
+	@Test
+	public void testValidParse_quotesInValue() {
+		def dson = '{{name="Tom \\\"Unit\\\" Test"},{age="20"},{hourlyRate="10.50"}}'
+		println dson
+		def map = new DsonParser(dson).parse()
+
+		Assert.assertEquals(3, map.size())
+		Assert.assertEquals('Tom \"Unit\" Test', map.name)
+		Assert.assertEquals('20', map.age)
+		Assert.assertEquals('10.50', map.hourlyRate)
+	}
+
 	@Test(expected=DsonParseException.class)
 	public void testInvalidParse_missingBrace() {
-		def dson = '{{name="Tom Test"},age=20},{hourlyRate=10.50}}'
+		def dson = '{{name="Tom Test"},age="20"},{hourlyRate="10.50"}}'
 		new DsonParser(dson).parse()
 	}
 
 	@Test(expected=DsonParseException.class)
 	public void testInvalidParse_missingComma() {
-		def dson = '{{name="Tom Test"}{age=20},{hourlyRate=10.50}}'
+		def dson = '{{name="Tom Test"}{age="20"},{hourlyRate="10.50"}}'
+		new DsonParser(dson).parse()
+	}
+
+	@Test(expected=DsonParseException.class)
+	public void testInvalidParse_missingQuotes() {
+		def dson = '{{name="Tom Test"},{age=20},{hourlyRate="10.50"}}'
 		new DsonParser(dson).parse()
 	}
 }
